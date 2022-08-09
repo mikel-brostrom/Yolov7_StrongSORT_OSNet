a5bb9a5be7ab6e12fc95cdc81752708326af5ef6
mikel.brostrom <mikel.brostrom@cevt.se>
2022-08-09 08:08:18 +0200

    fix bug

--- track.py
+++ track.py
@@ -276,7 +276,7 @@ def run(
 
     # Print results
     t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
-    print(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS, %.1fms strong sort update per image at shape {(1, 3, *imgsz)}' % t)
+    print(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS, %.1fms strong sort update per image at shape {(1, 3, imgsz, imgsz)}' % t)
     if save_txt or save_vid:
         s = f"\n{len(list(save_dir.glob('tracks/*.txt')))} tracks saved to {save_dir / 'tracks'}" if save_txt else ''
         print(f"Results saved to {colorstr('bold', save_dir)}{s}")
